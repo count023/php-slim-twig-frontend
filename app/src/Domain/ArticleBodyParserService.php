9dc65679fd6831b92b8563622eb4ca563d013209
count023 <count023@freenet.de>
2018-07-30 03:29:54 +0200

    added headline correction in ArticleBodyParser; first code scratches for inline relations

--- app/src/Domain/ArticleBodyParserService.php
+++ app/src/Domain/ArticleBodyParserService.php
@@ -34,9 +34,26 @@ class ArticleBodyParserService {
      */
     public function parse(string $articleBody): string {
 
-        $this->logger->debug('BEFORE clearHtml: articleBody: {ab}', ['ab' => $articleBody]);
+        //$this->logger->debug('BEFORE clearHtml: articleBody: {ab}', ['ab' => $articleBody]);
         $articleBody = $this->clearHtml($articleBody);
-        $this->logger->debug('AFTER clearHtml: articleBody: {ab}', ['ab' => $articleBody]);
+        //$this->logger->debug('AFTER clearHtml: articleBody: {ab}', ['ab' => $articleBody]);
+
+        // separate the paragraphs:
+        $domDocument = new \DOMDocument();
+        $domDocument->loadHTML('<?xml encoding="utf-8" ?>' . $articleBody);
+        $domDocument->normalizeDocument();
+        if ($domDocument !== false) {
+            $paragraphs = $domDocument->getElementsByTagName('p');
+            foreach ($paragraphs as $paragraph) {
+                $this->logger->debug('paragraph: ', ['p' => $paragraph->C14N()]);
+            }
+        }
+        $xpath = new \DOMXPath($domDocument);
+        // returns a list of all links with rel=nofollow
+        $nlist = $xpath->query("//a[starts-with(@href, 'internal:')]");
+        foreach ($nlist as $anchor) {
+            $this->logger->debug('anchor with internal: link: ', ['a' => $anchor->C14N()]);
+        }
 
         return $articleBody;
     }
@@ -48,6 +65,7 @@ class ArticleBodyParserService {
     protected function clearHtml(string $articleBody): string {
         $articleBody = $this->removeScaytRubbish($articleBody);
         $articleBody = $this->replaceFqdnsByDocRootRelativeSlash($articleBody);
+        $articleBody = $this->correctHeadlineHierarchie($articleBody);
         return $articleBody;
     }
 
@@ -67,4 +85,15 @@ class ArticleBodyParserService {
     protected function replaceFqdnsByDocRootRelativeSlash(string $articleBody): string {
         return preg_replace('/https?\:\/\/[^\/]*\//', '/', $articleBody);
     }
+
+    protected function correctHeadlineHierarchie($articleBody): string {
+        $articleBody = preg_replace('/<h6([^>]*)>(.*?)<\/h6>/', "<p$1 data-hl=\"headline-level-7\"><strong>$2</strong></p>", $articleBody);
+        $articleBody = preg_replace('/<h5([^>]*)>(.*?)<\/h5>/', "<h6$1>$2</h6>", $articleBody);
+        $articleBody = preg_replace('/<h4([^>]*)>(.*?)<\/h4>/', "<h5$1>$2</h5>", $articleBody);
+        $articleBody = preg_replace('/<h3([^>]*)>(.*?)<\/h3>/', "<h4$1>$2</h4>", $articleBody);
+        $articleBody = preg_replace('/<h2([^>]*)>(.*?)<\/h2>/', "<h3$1>$2</h3>", $articleBody);
+        $articleBody = preg_replace('/<h1([^>]*)>(.*?)<\/h1>/', "<h3$1>$2</h3>", $articleBody);
+        return $articleBody;
+    }
+
 }
